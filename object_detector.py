c89cceb6c8d278fd29dbf0b19630c1c24b577299
Kayla-Jade Butkow <kaylabutkow@gmail.com>
2019-07-01 12:19:29 +0200

    Initialisations

--- object_detector.py
+++ object_detector.py
@@ -14,3 +14,18 @@ ap.add_argument("-m", "--model", required=True,
 ap.add_argument("-c", "--confidence", type=float, default = 0.2,
                 help="minimum probability to filter weak detections")
 args = vars(ap.parse_args())
+
+prototxt = args["prototxt"]
+caffeModel = args["model"]
+imagePath = args["image"]
+
+imageWidth = 300
+imageHeight = 300
+
+# Initialize the class labels for MobileNet SSD
+CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
+	"bottle", "bus", "car", "cat", "chair", "cow", "dining table",
+	"dog", "horse", "motorbike", "person", "pot plant", "sheep",
+	"sofa", "train", "tv monitor"]
+# Generate colors for the bounding boxes for each class
+COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
